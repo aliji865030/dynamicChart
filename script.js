593dea467b1ff3b1d398fb5eba4385e1f1b38702
aliji865030 <82137372+aliji865030@users.noreply.github.com>
2024-02-03 21:12:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const data = {
   ],
   datasets: [
     {
-      label: "Ismail Salary",
+      label: "Abbas Salary",
       data: [65, 59, 80, 81, 56, 55, 40, 85, 30, 75, 49],
       fill: false,
       borderColor: "rgb(75, 192, 192)",
